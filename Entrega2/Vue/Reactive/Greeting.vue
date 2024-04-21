166e7976cb54d4843cfd46283ed51490bffda133
IvanSantoveniaMantecon <102520821+IvanSantoveniaMantecon@users.noreply.github.com>
2024-04-21 14:23:26 +0200

    Update Greeting.vue

--- Entrega2/Vue/Reactive/Greeting.vue
+++ Entrega2/Vue/Reactive/Greeting.vue
@@ -3,26 +3,12 @@
 </template>
 
 <script>
-import { computed, inject } from 'vue';
+import { inject } from 'vue';
 
 export default {
   setup() {
-    // Importamos el objeto reactivo `state` del componente padre
-    const state = inject('state');
-
-    // Variable computada para calcular el saludo
-    const greeting = computed(() => {
-      switch (state.selectedLanguage) {
-        case 'es':
-          return `¡Hola, ${state.name}!`;
-        case 'en':
-          return `Hello, ${state.name}!`;
-        case 'fr':
-          return `Bonjour, ${state.name}!`;
-        default:
-          return `¡Hola, ${state.name}!`;
-      }
-    });
+    // Importamos la variable computada `greeting` del componente padre
+    const greeting = inject('greeting');
 
     return {
       greeting

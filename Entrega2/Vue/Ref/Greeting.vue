d8fb50537eb0c64223174583215f5afbf8d3f236
IvanSantoveniaMantecon <102520821+IvanSantoveniaMantecon@users.noreply.github.com>
2024-04-21 14:24:39 +0200

    Update Greeting.vue

--- Entrega2/Vue/Ref/Greeting.vue
+++ Entrega2/Vue/Ref/Greeting.vue
@@ -3,20 +3,7 @@
 </template>
 
 <script>
-import { ref, watchEffect } from 'vue';
-import { computed } from 'vue';
-
 export default {
-  setup() {
-    // Importamos la variable reactiva `name` del componente padre
-    const { name } = inject('name');
-
-    // Variable reactiva para almacenar el saludo
-    const greeting = computed(() => `¡Hola, ${name.value}!`);
-
-    return {
-      greeting
-    };
-  }
+  props: ['greeting']
 };
 </script>

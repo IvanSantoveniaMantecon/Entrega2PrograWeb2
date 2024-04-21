ff78a5d2e3b1b23c446828dc351f8b7101dcce5e
IvanSantoveniaMantecon <102520821+IvanSantoveniaMantecon@users.noreply.github.com>
2024-04-21 14:25:52 +0200

    Update Greeting.vue

--- Entrega2/Vue/Watch/Greeting.vue
+++ Entrega2/Vue/Watch/Greeting.vue
@@ -3,16 +3,7 @@
 </template>
 
 <script>
-import { ref, inject } from 'vue';
-
 export default {
-  setup() {
-    // Importa la variable reactiva `greeting` del componente padre
-    const greeting = inject('greeting');
-
-    return {
-      greeting
-    };
-  }
+  props: ['greeting']
 };
 </script>
